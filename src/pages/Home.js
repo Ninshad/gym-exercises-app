b415b796e25a53180b00053989f3b4658aeb933f
Ninshad <ninshad7@gmail.com>
2022-12-12 18:27:33 +0530

    12/12/22

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import HeroBanner from '../components/HeroBanner'
+import SearchExercises from '../components/SearchExercises'
+
+const Home = () => {
+  return (
+    <>
+    <HeroBanner />
+    <SearchExercises />
+    </>
+  )
+}
+
+export default Home
